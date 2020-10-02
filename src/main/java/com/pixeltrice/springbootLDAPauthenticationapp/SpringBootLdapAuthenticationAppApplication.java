10b5454e7ddcc8108c93604937b8665b908dccb4
shubham kumar <shubhamkumar1980@gmail.com>
2020-10-02 14:49:44 +0530

    first commit

---
+++ src/main/java/com/pixeltrice/springbootLDAPauthenticationapp/SpringBootLdapAuthenticationAppApplication.java
@@ -0,0 +1,13 @@
+package com.pixeltrice.springbootLDAPauthenticationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLdapAuthenticationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLdapAuthenticationAppApplication.class, args);
+	}
+
+}
